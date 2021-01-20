929ce12cbceca6c69a7ea448c181be4f56a98212
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-20 15:33:51 -0500

    .get functioning with knex

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -3,5 +3,6 @@ const db = require("../../data/dbConfig");
 module.exports = {
   getAccounts() {
     // code example
+    return db("accounts");
   },
 };
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,8 +1,18 @@
 const express = require("express");
 
-const db = require("../../data/dbConfig");
 const Account = require("./accounts-model");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  Account.getAccounts()
+    .then((accounts) => {
+      console.log(accounts);
+      res.status(200).json(accounts);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: err.message });
+    });
+});
 
 module.exports = router;
